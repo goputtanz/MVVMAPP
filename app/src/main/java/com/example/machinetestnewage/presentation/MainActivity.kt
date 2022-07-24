411cdd61f1b49717874c656b3df8aef234297d8e
acer <iamgopikrishnanpv@gmail.com>
2022-07-24 19:07:09 +0530

    base packages and base dependencies implemented 2407220707

--- app/src/main/java/com/example/machinetestnewage/presentation/MainActivity.kt
+++ app/src/main/java/com/example/machinetestnewage/presentation/MainActivity.kt
@@ -1,11 +1,16 @@
-package com.example.machinetestnewage
+package com.example.machinetestnewage.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.machinetestnewage.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
     }
 }
