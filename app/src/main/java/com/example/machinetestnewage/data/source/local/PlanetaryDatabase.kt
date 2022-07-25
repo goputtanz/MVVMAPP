475ed790ea1aa03ac64e79915febe3c95dcd622c
Gopikrishnan.p.v <gopikrishnan.pv@macomsolutions.com>
2022-07-25 18:42:50 +0530

    some changes

--- app/src/main/java/com/example/machinetestnewage/data/source/local/PlanetaryDatabase.kt
+++ app/src/main/java/com/example/machinetestnewage/data/source/local/PlanetaryDatabase.kt
@@ -4,7 +4,6 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
-@TypeConverters(Converters::class)
 @Database(entities = [PlanetaryEntity::class], version = 1, exportSchema = false)
 abstract class PlanetaryDatabase :RoomDatabase(){
     abstract val planetaryDao: PlanetaryDao
